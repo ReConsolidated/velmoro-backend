d5923b419695a4765a74103ec39e51337131a769
ReConsolidated <gracjanpasik@gmail.com>
2024-09-03 09:18:02 +0200

    mvp done

--- src/main/java/io/github/reconsolidated/velmorobackend/infrastracture/rest/UserEventController.java
+++ src/main/java/io/github/reconsolidated/velmorobackend/infrastracture/rest/UserEventController.java
@@ -1,16 +1,12 @@
 package io.github.reconsolidated.velmorobackend.infrastracture.rest;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.github.reconsolidated.velmorobackend.application.UserEventService;
 import io.github.reconsolidated.velmorobackend.domain.event.EventLogRequestDto;
 import io.github.reconsolidated.velmorobackend.domain.event.UserEvent;
-import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/events")
@@ -24,7 +20,7 @@ public class UserEventController {
         return userEventService.logEvent(request.userId(), request.eventType(), request.details());
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping("/user/{userId}")
     public List<UserEvent> getUserEvents(@PathVariable String userId) {
         return userEventService.getUserEvents(userId);
     }
@@ -34,7 +30,7 @@ public class UserEventController {
         return userEventService.getAllEvents();
     }
 
-    @GetMapping("/{urlName}")
+    @GetMapping("/hotel/{urlName}")
     public List<UserEvent> getHotelEvents(@PathVariable String urlName) {
         return userEventService.getAllHotelEvents(urlName);
     }
